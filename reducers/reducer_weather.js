cf792ec383282301df62610c8fdbcc3838830f93
Juri <juri.l@me.com>
2017-08-28 19:23:33 +0300

    Delete reducer_weather.js

--- reducers/reducer_weather.js
+++
@@ -1,8 +0,0 @@
-// reducer to handle action, created by fetchWeather
-// reducer always takes arguments: 
-// state = null (defaulted to null)
-// action
-export default function ReducerWeather(state = null, action) {
-  console.log("Action received: ", action);
-  return state;
-}
