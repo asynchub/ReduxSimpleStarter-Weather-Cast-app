7b889e47d72898d862f089c99e459b7761fe0999
Juri <juri.l@me.com>
2017-08-28 19:23:07 +0300

    Delete index.js

--- reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import ReducerWeather from './reducer_weather';
-
-const rootReducer = combineReducers({
-  weather: ReducerWeather
-});
-
-export default rootReducer;
