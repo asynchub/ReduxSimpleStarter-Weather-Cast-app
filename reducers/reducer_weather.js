351f33d3785c4a2cf199a48fd75f3baa586268cd
Juri <juri.l@me.com>
2017-08-28 10:49:12 +0300

    Create reducer_weather.js

---
+++ reducers/reducer_weather.js
@@ -0,0 +1,8 @@
+// reducer to handle action, created by fetchWeather
+// reducer always takes arguments: 
+// state = null (defaulted to null)
+// action
+export default function reducerWeather(state = null, action) {
+  console.log("Action created: ", action);
+  return state;
+}
