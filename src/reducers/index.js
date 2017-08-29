6e09a774627258b70b8842ed5d3150f3c847493b
Juri <juri.l@me.com>
2017-08-29 16:49:44 +0300

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux';
 import ReducerWeather from './reducer_weather';
 
 const rootReducer = combineReducers({
+  // a piece of state, that will be sent to all components or containers
+  // after middleware processed it.
   weather: ReducerWeather
 });
 
